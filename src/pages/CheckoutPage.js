2dace6bf5bc464fe32b18cb5911e64962aa58ea7
shenry45 <shawn45henry@gmail.com>
2022-05-26 21:12:20 -0700

    copy billing to shipping button hides shipping fields, notes field added, styling

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -12,7 +12,7 @@ class CheckoutPage extends React.Component {
             billingAddress: '',
             billingAddress2: '',
             billingCity: '',
-            billingState: '',
+            billingState: 'AL',
             billingPostalCode: '',
             billingPhone: '',
             billingEmail: '',
@@ -22,11 +22,10 @@ class CheckoutPage extends React.Component {
             shippingAddress: '',
             shippingAddress2: '',
             shippingCity: '',
-            shippingState: '',
+            shippingState: 'AL',
             shippingPostalCode: '',
-            shippingPhone: '',
-            shippingEmail: '',
-            notes: ''
+            notes: '',
+            copyBilltoShip: false
         }
 
         this.FormChangeHandler = this.FormChangeHandler.bind(this);
@@ -42,7 +41,15 @@ class CheckoutPage extends React.Component {
     }
 
     CopyBillingToShipping(e) {
+        // console.log(e);
+
         if (!e.target.checked) {
+            this.setState((state) => {
+                return {
+                    ...state,
+                    copyBilltoShip: false
+                }
+            });
             return;
         }
 
@@ -55,7 +62,10 @@ class CheckoutPage extends React.Component {
             }
         }
 
-        this.setState(state);
+        this.setState({
+            ...state,
+            copyBilltoShip: true
+        });
     }
 
     render() {
@@ -76,14 +86,14 @@ class CheckoutPage extends React.Component {
                                 <input type="text" maxLength="15" name="billingLastName" onChange={this.FormChangeHandler} required />
                             </div>
                         </div>
-                        <label htmlFor="billing-company">Company</label>
+                        <label htmlFor="billingCompany">Company</label>
                         <input type="text" maxLength="50" name="billingCompany" onChange={this.FormChangeHandler} value={this.state.billingCompany} />
-                        <label htmlFor="billing-address">Street Address</label>
+                        <label htmlFor="billingAddress">Street Address</label>
                         <input type="text" maxLength="45" name="billingAddress" onChange={this.FormChangeHandler} value={this.state.billingAddress} required />
-                        <label htmlFor="billing-address2">Suite / Unit</label>
+                        <label htmlFor="billingAddress2">Suite / Unit</label>
                         <input type="text" maxLength="15" name="billingAddress2" onChange={this.FormChangeHandler} value={this.state.billingAddress2} />
-                        <label htmlFor="billing-state">State</label>
-                        <select name="billing-state" onChange={this.FormChangeHandler}>
+                        <label htmlFor="billingState">State</label>
+                        <select name="billingState" onChange={this.FormChangeHandler}>
                             <option value="AL">Alabama</option>
                             <option value="AK">Alaska</option>
                             <option value="AZ">Arizona</option>
@@ -156,87 +166,83 @@ class CheckoutPage extends React.Component {
                             <input type="checkbox" name="same-as-billing" id="same-as-billing" onChange={this.CopyBillingToShipping} />
                             <label htmlFor="same-as-billing">Same as billing address</label>
                         </div>
-                        <div>
-                            <div className='stacked'>
-                                <label htmlFor="shippingFirstName">First Name</label>
-                                <input type="text" maxLength="15" name="shippingFirstName" value={this.state.shippingFirstName} onChange={this.FormChangeHandler} required />
+                        <div className={`${this.state.copyBilltoShip ? 'hidden' : ''} shipping-cont`}>
+                            <div>
+                                <div className='stacked'>
+                                    <label htmlFor="shippingFirstName">First Name</label>
+                                    <input type="text" maxLength="15" name="shippingFirstName" value={this.state.shippingFirstName} onChange={this.FormChangeHandler} required />
+                                </div>
+                                <div className='stacked'>
+                                    <label htmlFor="shippingLastName">Last Name</label>
+                                    <input type="text" maxLength="15" name="shippingLastName" value={this.state.shippingLastName} onChange={this.FormChangeHandler} required />
+                                </div>
                             </div>
-                            <div className='stacked'>
-                                <label htmlFor="shippingLastName">Last Name</label>
-                                <input type="text" maxLength="15" name="shippingLastName" value={this.state.shippingLastName} onChange={this.FormChangeHandler} required />
-                            </div>
+                            <label htmlFor="shippingCompany">Company</label>
+                            <input type="text" maxLength="50" name="shippingCompany" value={this.state.shippingCompany} onChange={this.FormChangeHandler} />
+                            <label htmlFor="shippingAddress">Street Address</label>
+                            <input type="text" maxLength="45" name="shippingAddress" value={this.state.shippingAddress} onChange={this.FormChangeHandler} required />
+                            <label htmlFor="shippingAddress2">Suite / Unit</label>
+                            <input type="text" maxLength="15" name="shippingAddress2" value={this.state.shippingAddress2} onChange={this.FormChangeHandler} />
+                            <label htmlFor="shippingState">State</label>
+                            <select name="shippingState" value={this.state.shippingState} onChange={this.FormChangeHandler}>
+                                <option value="AL">Alabama</option>
+                                <option value="AK">Alaska</option>
+                                <option value="AZ">Arizona</option>
+                                <option value="AR">Arkansas</option>
+                                <option value="CA">California</option>
+                                <option value="CO">Colorado</option>
+                                <option value="CT">Connecticut</option>
+                                <option value="DE">Delaware</option>
+                                <option value="DC">District Of Columbia</option>
+                                <option value="FL">Florida</option>
+                                <option value="GA">Georgia</option>
+                                <option value="HI">Hawaii</option>
+                                <option value="ID">Idaho</option>
+                                <option value="IL">Illinois</option>
+                                <option value="IN">Indiana</option>
+                                <option value="IA">Iowa</option>
+                                <option value="KS">Kansas</option>
+                                <option value="KY">Kentucky</option>
+                                <option value="LA">Louisiana</option>
+                                <option value="ME">Maine</option>
+                                <option value="MD">Maryland</option>
+                                <option value="MA">Massachusetts</option>
+                                <option value="MI">Michigan</option>
+                                <option value="MN">Minnesota</option>
+                                <option value="MS">Mississippi</option>
+                                <option value="MO">Missouri</option>
+                                <option value="MT">Montana</option>
+                                <option value="NE">Nebraska</option>
+                                <option value="NV">Nevada</option>
+                                <option value="NH">New Hampshire</option>
+                                <option value="NJ">New Jersey</option>
+                                <option value="NM">New Mexico</option>
+                                <option value="NY">New York</option>
+                                <option value="NC">North Carolina</option>
+                                <option value="ND">North Dakota</option>
+                                <option value="OH">Ohio</option>
+                                <option value="OK">Oklahoma</option>
+                                <option value="OR">Oregon</option>
+                                <option value="PA">Pennsylvania</option>
+                                <option value="RI">Rhode Island</option>
+                                <option value="SC">South Carolina</option>
+                                <option value="SD">South Dakota</option>
+                                <option value="TN">Tennessee</option>
+                                <option value="TX">Texas</option>
+                                <option value="UT">Utah</option>
+                                <option value="VT">Vermont</option>
+                                <option value="VA">Virginia</option>
+                                <option value="WA">Washington</option>
+                                <option value="WV">West Virginia</option>
+                                <option value="WI">Wisconsin</option>
+                                <option value="WY">Wyoming</option>
+                            </select>
+                            <label htmlFor="shippingPostalCode">Postal Code</label>
+                            <input type="text" maxLength="7" name="shippingPostalCode" value={this.state.shippingPostalCode} onChange={this.FormChangeHandler} required />
                         </div>
-                        <label htmlFor="shippingCompany">Company</label>
-                        <input type="text" maxLength="50" name="shippingCompany" value={this.state.shippingCompany} onChange={this.FormChangeHandler} />
-                        <label htmlFor="shippingAddress">Street Address</label>
-                        <input type="text" maxLength="45" name="shippingAddress" value={this.state.shippingAddress} onChange={this.FormChangeHandler} required />
-                        <label htmlFor="shippingAddress2">Suite / Unit</label>
-                        <input type="text" maxLength="15" name="shippingAddress2" value={this.state.shippingAddress2} onChange={this.FormChangeHandler} />
-                        <label htmlFor="shippingState">State</label>
-                        <select name="shippingState">
-                            <option value="AL">Alabama</option>
-                            <option value="AK">Alaska</option>
-                            <option value="AZ">Arizona</option>
-                            <option value="AR">Arkansas</option>
-                            <option value="CA">California</option>
-                            <option value="CO">Colorado</option>
-                            <option value="CT">Connecticut</option>
-                            <option value="DE">Delaware</option>
-                            <option value="DC">District Of Columbia</option>
-                            <option value="FL">Florida</option>
-                            <option value="GA">Georgia</option>
-                            <option value="HI">Hawaii</option>
-                            <option value="ID">Idaho</option>
-                            <option value="IL">Illinois</option>
-                            <option value="IN">Indiana</option>
-                            <option value="IA">Iowa</option>
-                            <option value="KS">Kansas</option>
-                            <option value="KY">Kentucky</option>
-                            <option value="LA">Louisiana</option>
-                            <option value="ME">Maine</option>
-                            <option value="MD">Maryland</option>
-                            <option value="MA">Massachusetts</option>
-                            <option value="MI">Michigan</option>
-                            <option value="MN">Minnesota</option>
-                            <option value="MS">Mississippi</option>
-                            <option value="MO">Missouri</option>
-                            <option value="MT">Montana</option>
-                            <option value="NE">Nebraska</option>
-                            <option value="NV">Nevada</option>
-                            <option value="NH">New Hampshire</option>
-                            <option value="NJ">New Jersey</option>
-                            <option value="NM">New Mexico</option>
-                            <option value="NY">New York</option>
-                            <option value="NC">North Carolina</option>
-                            <option value="ND">North Dakota</option>
-                            <option value="OH">Ohio</option>
-                            <option value="OK">Oklahoma</option>
-                            <option value="OR">Oregon</option>
-                            <option value="PA">Pennsylvania</option>
-                            <option value="RI">Rhode Island</option>
-                            <option value="SC">South Carolina</option>
-                            <option value="SD">South Dakota</option>
-                            <option value="TN">Tennessee</option>
-                            <option value="TX">Texas</option>
-                            <option value="UT">Utah</option>
-                            <option value="VT">Vermont</option>
-                            <option value="VA">Virginia</option>
-                            <option value="WA">Washington</option>
-                            <option value="WV">West Virginia</option>
-                            <option value="WI">Wisconsin</option>
-                            <option value="WY">Wyoming</option>
-                        </select>
-                        <label htmlFor="shippingPostalCode">Postal Code</label>
-                        <input type="text" maxLength="7" name="shippingPostalCode" value={this.state.shippingPostalCode} onChange={this.FormChangeHandler} required />
-                        <div>
-                            <div className='stacked'>
-                                <label htmlFor="shippingPhone">Phone Number</label>
-                                <input type="tel" maxLength="7" name="shippingPhone" value={this.state.shippingPhone} onChange={this.FormChangeHandler} required />
-                            </div>
-                            <div className='stacked'>
-                                <label htmlFor="shippingEmail">Email Address</label>
-                                <input type="email" maxLength="40" name="shippingEmail" value={this.state.shippingEmail} onChange={this.FormChangeHandler} required />
-                            </div>
+                        <div className='notes-cont'>
+                            <label htmlFor="notes">Notes</label>
+                            <textarea id="notes" name="notes"></textarea>
                         </div>
                     </div>
                 </form>
